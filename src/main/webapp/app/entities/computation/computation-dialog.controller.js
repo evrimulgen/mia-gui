6fa49b510ab94c713c58fbb6495a47886cc6f6a4
Sander Puts <putssander@gmail.com>
2016-04-13 18:19:58 +0200

    MAMAMIA-14 saving configstring works loading not

--- src/main/webapp/app/entities/computation/computation-dialog.controller.js
+++ src/main/webapp/app/entities/computation/computation-dialog.controller.js
@@ -11,10 +11,13 @@
         var vm = this;
         vm.computation = entity;
         vm.volumeofinterests = VolumeOfInterest.query();
+        vm.selectedModule = {};
         vm.load = function(id) {
             Computation.get({id : id}, function(result) {
                 vm.computation = result;
             });
+        	console.log("load")
+        	entity.computationConfiguration = JSON.parse(vm.computation.computationConfiguration)
         };
 
         var onSaveSuccess = function (result) {
@@ -29,6 +32,8 @@
 
         vm.save = function () {
             vm.isSaving = true;
+            vm.computation.computationConfiguration = JSON.stringify(vm.configuredComputation);
+
             if (vm.computation.id !== null) {
                 Computation.update(vm.computation, onSaveSuccess, onSaveError);
             } else {
@@ -39,5 +44,26 @@
         vm.clear = function() {
             $uibModalInstance.dismiss('cancel');
         };
+        
+        vm.selectedModuleName = function(){
+        	return vm.selectedModule.moduleName;
+        }
+        
+        vm.modules = [
+        {
+        	moduleName: "DoseComputation",
+        	moduleOperations: ["min","mean","max"]
+        },
+        {
+        	moduleName: "DvhVolumeComputation",
+        	moduleOperations: ["min","mean","max"],
+        	absoluteOutput: ["true","false"],
+        	limit: "integer",
+        	volumeType: ["%", "cc"],
+        	targetPrescriptionDose: "NaN"
+        },
+        
+        ];
+        
     }
 })();
